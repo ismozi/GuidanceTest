b48ebef7d1c7d59e624becb257a255fcbbb36cea
ismozi <61505183+ismozi@users.noreply.github.com>
2023-05-28 17:50:28 -0400

    modifs

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ import time
 
 # set the default language model used to execute guidance programs
 # mpt = guidance.llms.transformers.MPT('mosaicml/mpt-7b-instruct', device=0)
-openllama = guidance.llms.transformers.LLaMA('openlm-research/open_llama_3b_600bt_preview', device=0)
-# incite = openllama = guidance.llms.Transformers('togethercomputer/RedPajama-INCITE-Instruct-3B-v1', device=0)
+# openllama = guidance.llms.transformers.LLaMA('openlm-research/open_llama_3b_600bt_preview', device=0)
+incite = guidance.llms.Transformers('togethercomputer/RedPajama-INCITE-Instruct-3B-v1', device=0)
 
 while (True):
   mem = input('')
@@ -12,6 +12,6 @@ while (True):
   with open('prompt.txt', 'r') as file:
     prompt = file.read()
   program = guidance(prompt)
-  executed_program = program(memory=mem, llm=openllama)
+  executed_program = program(memory=mem, llm=incite)
   print(executed_program)
   print(f'time: ${time.time() - starttime}')
